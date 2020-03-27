ab7e7e7f37cb48459ef7a1cc2e3198116c1d2de1
ZhanKanyun <50827428+ZhanKanyun@users.noreply.github.com>
2020-03-27 14:03:38 +0100

    Update and rename fetchimpl.groovy to FetchImpl.groovy
    Camel-Case

--- src/main/groovy/gl51/service/Impl/FetchImpl.groovy
+++ src/main/groovy/gl51/service/Impl/FetchImpl.groovy
@@ -2,7 +2,7 @@ package gl51.service.lmpl
 
 import gl51.data.photo
 import gl51.service.fetch
-class fetchimpl implements fetch {
+class FetchImpl implements fetch {
     photo fetch() {
         return null
     }
